10f0bd0e1bb5cf4fa00d7c178ebeb93e761ee2ab
Thomas Mailund <mailund@birc.au.dk>
2017-06-08 15:05:57 +0200

    renamed build_conda to deploy_conda

--- build_conda.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Builds and uploads conda packages for the current Python version and
-# all supported platforms (osx, win, linux).
-
-set -eu
-set -o pipefail
-
-anaconda -t $ANACONDA_TOKEN upload --user mailund $HOME/miniconda/conda-bld/*/*.tar.bz2
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="statusbar",
-    version="0.1.20",
+    version="0.1.21",
 
     packages=find_packages(),
 
