dd44a1f577b8f23eac28c28298c0b3991e335816
Thomas Mailund <mailund@birc.au.dk>
2017-06-08 14:22:54 +0200

    trying different approach to anaconda builds...

--- build_conda.sh
+++ build_conda.sh
@@ -3,27 +3,7 @@
 # Builds and uploads conda packages for the current Python version and
 # all supported platforms (osx, win, linux).
 
-set -e
+set -eu
+set -o pipefail
 
-cd /tmp
-
-# Setup Miniconda...
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p /tmp/miniconda
-export PATH="/tmp/miniconda/bin:$PATH"
-
-# Configure and update...
-conda config --set always_yes yes --set changeps1 no
-conda config --add channels omnia
-conda update -q conda
-conda install anaconda-client conda-build
-
-# Build skeleton from PyPI package...
-conda skeleton pypi --pypi-url $PYPI_URL statusbar
-conda build --python $TRAVIS_PYTHON_VERSION statusbar
-mkdir /tmp/builds
-conda convert --platform all /tmp/miniconda/conda-bld/linux-64/statusbar-*-*_0.tar.bz2 -o /tmp/builds
-
-for f in $(ls /tmp/builds/*/*.tar.bz2); do
-    anaconda --user mailund -t $ANACONDA_TOKEN upload $f
-done
+anaconda -t $ANACONDA_TOKEN upload --user mailund $HOME/miniconda/conda-bld/*/*.tar.bz2
